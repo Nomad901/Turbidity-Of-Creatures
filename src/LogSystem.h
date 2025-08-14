0a06f654f86f2e0ca184e918d2690c03c13df575
Tango <maxim.ant07@gmail.com>
2025-08-14 20:41:57 +0300

    Update LogSystem.h

--- src/LogSystem.h
+++ src/LogSystem.h
@@ -8,9 +8,18 @@
 
 class LogSystem
 {
+public:	
+	enum class typeInfo
+	{
+		INFO = 0,
+		WARNING = 1,
+		ERROR = 2,
+		SERIOUS_ERROR = 3,
+		ERROR_TO_DEATH = 4
+	};
 public:
-	enum class typeInfo;
-public:
+	static LogSystem& getInstance();
+ 
 	auto getInfo(typeInfo pType, std::string_view pMsg,
 				 std::string_view pFile, int32_t pLine) const noexcept -> const std::string&;
 	auto getWarning(typeInfo pType, std::string_view pMsg,
@@ -19,37 +28,35 @@ public:
 				  std::string_view pFile, int32_t pLine) const noexcept -> const std::string&;
 
 protected:
-	enum class typeInfo
-	{
-		INFO = 0,
-		WARNING = 1,
-		ALMOST_ERROR_WARNING = 2,
-		ERROR = 3,
-		HUGE_ERROR = 4,
-		SERIOUS_ERROR = 5,
-		VERY_SERIOUS_ERROR = 6,
-		ERROR_TO_DEATH = 7
-	};
+	LogSystem() = default;
+	~LogSystem();
+
+private:
+	LogSystem(const LogSystem&) = delete;
+	LogSystem operator=(const LogSystem&) = delete;
+	LogSystem(LogSystem&&) = delete;
+	LogSystem operator=(LogSystem&&) = delete;
 
 private:
 	FileSystem mFileSystem;
-	LogSystem* mInstance;
+	static LogSystem* mInstance;
 
-} gLogSystem;
+};
 
+// LogSystem::typeInfo for the pType
 #define LOG(pType, pMsg) \
     do \
 	{ \
         switch (pType) \
 		{ \
-            case gLogSystem.typeInfo::INFO: \
-                gLogSystem.getInfo(pType, pMsg, __FILE__, __LINE__); \
+            case LogSystem::typeInfo::INFO: \
+                LogSystem::getInstance().getInfo(pType, pMsg, __FILE__, __LINE__); \
                 break; \
-            case gLogSystem.typeInfo::WARNING: \
-                gLogSystem.getWarning(pType, pMsg, __FILE__, __LINE__); \
+            case LogSystem::typeInfo::WARNING: \
+                LogSystem::getInstance().getWarning(pType, pMsg, __FILE__, __LINE__); \
                 break; \
             default: \
-                gLogSystem.getError(pType, pMsg, __FILE__, __LINE__); \
+                LogSystem::getInstance().getError(pType, pMsg, __FILE__, __LINE__); \
                 break; \
 		} \
     } while (0) 
