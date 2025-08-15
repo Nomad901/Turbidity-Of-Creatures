1a9e215beab74c37ef2998c2561c723bcf8f7713
Tango <maxim.ant07@gmail.com>
2025-08-15 20:14:51 +0300

    Update LogSystem.cpp

--- src/LogSystem.cpp
+++ src/LogSystem.cpp
@@ -14,24 +14,69 @@ LogSystem& LogSystem::getInstance()
 }
 
 auto LogSystem::getInfo(typeInfo pType, std::string_view pMsg,
-						std::string_view pFile, int32_t pLine) const noexcept -> const std::string&
+						std::string_view pFile, int32_t pLine) -> std::string
 {
-	
-	return "";
+	std::string consoleText = std::format("[TYPE] {} [MESSAGE] {} [FILE] {} [LINE] {} [TIME] {}\n", 
+										   "Info", pMsg, pFile, pLine, std::chrono::system_clock::now());
+	std::cout << consoleText;
+	mFileSystem.writeFile("Log", consoleText);
+	return consoleText;
 }
 
 auto LogSystem::getWarning(typeInfo pType, std::string_view pMsg, 
-						   std::string_view pFile, int32_t pLine) const noexcept -> const std::string&
+						   std::string_view pFile, int32_t pLine) -> std::string
 {
-
-	return "";
+	std::string consoleText = std::format("[TYPE] {} [MESSAGE] {} [FILE] {} [LINE] {} [TIME] {}\n",
+										   "Warning", pMsg, pFile, pLine, std::chrono::system_clock::now());
+	std::cout << consoleText;
+	mFileSystem.writeFile("Log", consoleText);
+	return consoleText;
 }
 
 auto LogSystem::getError(typeInfo pType, std::string_view pMsg, 
-						 std::string_view pFile, int32_t pLine) const noexcept -> const std::string&
+						 std::string_view pFile, int32_t pLine) -> std::string
 {
+	std::string consoleText = "";
+	switch (pType)
+	{
+	case LogSystem::typeInfo::ERROR:
+		consoleText = std::format("[TYPE] {} [MESSAGE] {} [FILE] {} [LINE] {} [TIME] {}\n",
+								  "Error", pMsg, pFile, pLine, std::chrono::system_clock::now());
+		break;
+	case LogSystem::typeInfo::SERIOUS_ERROR:
+		consoleText = std::format("[TYPE] {} [MESSAGE] {} [FILE] {} [LINE] {} [TIME] {}\n",
+								  "Serious Error", pMsg, pFile, pLine, std::chrono::system_clock::now());
+		break;
+	case LogSystem::typeInfo::ERROR_TO_DEATH:
+		consoleText = std::format("[TYPE] {} [MESSAGE] {} [FILE] {} [LINE] {} [TIME] {}\n",
+								  "Error to death", pMsg, pFile, pLine, std::chrono::system_clock::now());
+		break;
+	default:
+		break;
+	}
+	std::cout << consoleText;
+	mFileSystem.writeFile("Log", consoleText);
+	if(pType == typeInfo::ERROR_TO_DEATH)
+		std::abort();
+	return consoleText;
+}
 
-	return "";
+auto LogSystem::clearLogFile() -> void
+{
+	mFileSystem.clearFile("Log");
+}
+
+auto LogSystem::getLogFile() -> std::string
+{
+	std::string logFile = mFileSystem.readFileToString("Log");
+	std::cout << std::format("Log: {}\n", logFile);
+	return logFile;
+}
+
+LogSystem::LogSystem()
+{
+	std::string resource_path = RESOURCES_PATH;
+	mFileSystem.openFile("Log", resource_path + "log.txt", std::ios::app);
 }
 
 LogSystem::~LogSystem()
